f6eb29e16cf6a2c00e359bad883f7e6f59a0a98a
Adrian Stutz <adrian@sttz.ch>
2017-09-24 19:07:34 +0200

    Update comments

--- Base/Option.cs
+++ Base/Option.cs
@@ -33,24 +33,72 @@ public abstract class Option : IOption
 	/// </summary>
 	public const string DEFINE_PREFIX = "OPTION_";
 
+	/// <summary>
+	/// Wether the Option is build-only.
+	/// </summary>
+	/// <remarks>
+	/// Build-only options only apply to the build process.
+	/// They're not loaded at runtime in the editor or player
+	/// and their <c>Apply</c> method is only called at build-time.
+	/// </remarks>
 	public bool BuildOnly { get; private set; }
 
+	/// <summary>
+	/// Perform removal of option during build.
+	/// </summary>
+	/// <remarks>
+	/// Overwrite this method to do custom processing when the
+	/// option is not included in a build. This method gets called
+	/// during each built scene's <c>OnPostprocessScene</c>.
+	/// </remarks>
 	public virtual void PostprocessScene(Scene scene, bool isBuild, bool includedInBuild, Profile profile)
 	{
 		// NOP
 	}
 
+	/// <summary>
+	/// The priority of the option's <c>PostProcessBuild</c> callback.
+	/// </summary>
+	/// <remarks>
+	/// This determines the order in which all options' <c>PostProcessBuild</c> 
+	/// method is called. Lower values are called first.<br>
+	/// Note that this only orders the options between themselves, this does
+	/// not affect other <c>PostProcessBuild</c> callbacks.
+	/// </remarks>
 	public int PostprocessOrder { get; protected set; }
 
+	/// <summary>
+	/// Do custom processing before the build is started.
+	/// </summary>
+	/// <param name="target">Build target type</param>
+	/// <param name="path">Path to the built project</param>
+	/// <param name="includedInBuild">Wether this option should be included in build</param>
+	/// <param name="profile">The profile used in the build</param>
 	public virtual void PreprocessBuild(BuildTarget target, string path, bool includedInBuild, Profile profile)
 	{
 		// NOP
 	}
 
+	/// <summary>
+	/// Do custom processing after the build has been completed.
+	/// </summary>
+	/// <param name="target">Build target type</param>
+	/// <param name="path">Path to the built project</param>
+	/// <param name="includedInBuild">Wether this option should be included in build</param>
+	/// <param name="profile">The profile used in the build</param>
 	public virtual void PostprocessBuild(BuildTarget target, string path, bool includedInBuild, Profile profile)
 	{
 		// NOP
 	}
+
+	/// <summary>
+	/// The scripting define symbols set by this option.
+	/// </summary>
+	/// <remarks>
+	/// This includes <c>DEFINE_PREFIX + Name</c> by default when the
+	/// option is included in the build. Overrride this method to add 
+	/// custom defines or change when the define is set.
+	/// </remarks>
 	public virtual IEnumerable<string> GetSctiptingDefineSymbols(bool includedInBuild, string parameter, string value)
 	{
 		if (includedInBuild) {
@@ -60,12 +108,23 @@ public abstract class Option : IOption
 		}
 	}
 
+	/// <summary>
+	/// Do the editor GUI to edit this option.
+	/// </summary>
+	/// <remarks>
+	/// Use one of the typed subclasses that already provides a GUI
+	/// and/or override this method to customize how the option is
+	/// edited.
+	/// </remarks>
 	public abstract string EditGUI(GUIContent label, string input);
 
 	#endif
 
 	/// <summary>
+	/// The name of the option, used in config files, shown in the editor
+	/// and used for the scripting define symbol.
 	/// </summary>
+	/// <returns></returns>
 	public abstract string Name { get; }
 
 	public IOption Parent {
@@ -105,9 +164,25 @@ public abstract class Option : IOption
 		}
 	}
 	private string _defaultValue = string.Empty;
+
+	/// <summary>
+	/// Parse and load an input string.
+	/// </summary>
 	public abstract void Load(string input);
+	/// <summary>
+	/// Serialize the option's value to a string.
+	/// </summary>
 	public abstract string Save();
 
+	/// <summary>
+	/// Apply the option to the current environment.
+	/// </summary>
+	/// <remarks>
+	/// This method gets called when playing in the editor or at runtime,
+	/// on load and then every time the option's value changes.<br>
+	/// Options that have the [ExecuteInEditMode] attribute set will also
+	/// get called at edit-time.
+	/// </remarks>
 	public virtual void Apply()
 	{
 		if (variants != null) {
@@ -145,13 +220,44 @@ public abstract class Option : IOption
 
 	// -------- Variants --------
 
+	/// <summary>
+	/// Wether the option accepts variants.
+	/// </summary>
 	public bool IsVariant { get; protected set; }
+	/// <summary>
+	/// The parameter of a variant option, only valid if <c>IsVariant</c> is <c>true</c>.
+	/// </summary>
+	/// <remarks>
+	/// The variant parameter should not be changed after the option has been initialized.
+	/// The system will remove/create new instances for different parameters.
+	/// </remarks>
 	public string VariantParameter { get; set; }
+	/// <summary>
+	/// The default value of the parameter of a variant option.
+	/// </summary>
 	public string VariantDefaultParameter { get; protected set; }
+	/// <summary>
+	/// Wether this option instance represents the variant with the default parameter.
+	/// </summary>
+	/// </remarks>
+	/// Variant options can have an arbitrary number instances, each with
+	/// a different variant parameter to distinguish them. Variant options are
+	/// created on-demand when a new paramter appears. However, the one
+	/// instance using the <c>VariantDefaultParameter</c> is guaranteed 
+	/// to always exist.
+	/// </remarks>
 	public bool IsDefaultVariant { get; set; }
 
 	private Dictionary<string, IOption> variants;
 
+	/// <summary>
+	/// All the variants of this option currently known.
+	/// Will return an empty enumerable except for the <c>IsDefaultVariant</c> option.
+	/// </summary>
+	/// <remarks>
+	/// Note that this list of variants won't include the default variant,
+	/// which acts as the container for the other variants.
+	/// </remarks>
 	public IEnumerable<IOption> Variants {
 		get {
 			if (variants == null) {
@@ -165,6 +271,13 @@ public abstract class Option : IOption
 	/// <summary>
 	/// Add a new variant option.
 	/// </summary>
+	/// <remarks>
+	/// Variants can only be added to a <c>IsVariant</c> option and also only
+	/// to the <c>IsDefaultVariant</c> instance that acts as the container for
+	/// the other variants. The default variant and all other variants must be
+	/// of the same type and each variant must have a unique non-null 
+	/// <c>VariantParameter</c>.
+	/// </remarks>
 	public IOption AddVariant(string parameter)
 	{
 		Assert.IsTrue(IsVariant, "Invalid call to AddVariant, option is not variant.");
@@ -186,6 +299,14 @@ public abstract class Option : IOption
 		return instance;
 	}
 
+	/// <summary>
+	/// Get the variant option for the given parameter.
+	/// </summary>
+	/// <remarks>
+	/// Variants are only available on a <c>IsVariant</c> option and also only
+	/// on the <c>IsDefaultVariant</c> instance that acts as the container for
+	/// the other variants.
+	/// </remarks>
 	public IOption GetVariant(string parameter)
 	{
 		Assert.IsTrue(IsVariant, "Invalid call to GetVariant, option is not variant.");
@@ -224,16 +345,30 @@ public abstract class Option : IOption
 
 	// -------- Children --------
 
+	/// <summary>
+	/// The types of the child options of this option.
+	/// </summary>
+	/// <remarks>
+	/// Child options are created automatically together with
+	/// the main option and allow an option to contain a set 
+	/// of values of different types.
+	/// </remarks>
 	public Type[] ChildOptionTypes { get; protected set; }
 
 	private Dictionary<string, IOption> children;
 
+	/// <summary>
+	/// Wether this option has children.
+	/// </summary>
 	public bool HasChildren {
 		get {
 			return children != null && children.Count > 0;
 		}
 	}
 
+	/// <summary>
+	/// The child instances of this option.
+	/// </summary>
 	public IEnumerable<IOption> Children {
 		get {
 			if (children == null) {
@@ -262,6 +397,9 @@ public abstract class Option : IOption
 		}
 	}
 
+	/// <summary>
+	/// Get a child option instance by its name.
+	/// </summary>
 	public IOption GetChild(string name)
 	{
 		if (children == null)
@@ -272,6 +410,14 @@ public abstract class Option : IOption
 
 	// -------- Category --------
 
+	/// <summary>
+	/// Category of the option, only used in the editor.
+	/// </summary>
+	/// <remarks>
+	/// The category is used to group options in the editor.
+	/// Only the main option can have a category, the value is
+	/// ignored for child or variant options.
+	/// </remarks>
 	public string Category { get; protected set; }
 
 	#if UNITY_EDITOR
