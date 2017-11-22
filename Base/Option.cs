dcb9d0834530cd8286c5bbfdebc5f7fd1dae52eb
Adrian Stutz <adrian@sttz.ch>
2017-11-22 19:52:06 +0100

    Add license

--- Base/Option.cs
+++ Base/Option.cs
@@ -1,4 +1,22 @@
-﻿#if !NO_TRIMMER || UNITY_EDITOR
+//
+// Trimmer Framework for Unity - https://sttz.ch/trimmer
+// Copyright © 2017 Adrian Stutz
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#if !NO_TRIMMER || UNITY_EDITOR
 
 // Workaround for docfx documentation building
 #if !UNITY_5 && !UNITY_2017 && !UNITY_2018
